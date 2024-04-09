6ba3ee37a0d33e75899cfdd6aaf56d77b2e9c59a
Mass <89718019+Mass-ms@users.noreply.github.com>
2024-04-09 19:39:04 +0900

    Add files via upload

---
+++ code/p66_static_typing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i = 100;
+    float f = 12.3;
+    string s = "Hello";
+    bool b = true;
+
+    cout << i << f << s << b << endl;
+}
